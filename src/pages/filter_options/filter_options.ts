65d54c03f59487842fbd44210149c5a29d5cf882
jorge52an <jorgesolavila@gmail.com>
2017-01-09 19:45:37 -0500

    Feat: complete filter options

--- src/pages/filter_options/filter_options.ts
+++ src/pages/filter_options/filter_options.ts
@@ -28,20 +28,6 @@ export class FilterPage
 
 	dismiss()
 	{
-		let types = {};
-		for( let i = 0; i < this.typeOptions.length; ++i )
-			types[this.typeOptions[i].value] = this.typeOptions[i].filter;
-
-		let events = [];
-		for( let i = 0; i < this.eventService.events.length; ++i )
-			events.push( this.eventService.events[i] );
-		for( let i = 0; i < events.length; ++i )
-			if( !types[events[i].type] )
-			{
-				events.splice( i, 1 );
-				--i;
-			}
-
-		this.viewCtrl.dismiss( { filterOptions: this.filterOptions, typeOptions: this.typeOptions, distance: this.distance, date: this.eventDate, events: events } );
+		this.viewCtrl.dismiss( { filterOptions: this.filterOptions, typeOptions: this.typeOptions, distance: this.distance, date: this.eventDate } );
 	}
 }
--- src/pages/near_events/near_events.ts
+++ src/pages/near_events/near_events.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, ElementRef } from "@angular/core";
-
-import { NavController, Platform, ModalController } from "ionic-angular";
+import { TranslateService } from "ng2-translate";
+import { NavController, Platform, ModalController, LoadingController } from "ionic-angular";
 import { Geolocation } from "ionic-native";
 
 import { Event, EventService } from "../../services/event.service";
@@ -16,11 +16,11 @@ export class NearEventsPage
 {
 	map: any;
 	markers = new Array();
-	latLng: any;
+	currentPosition: any;
 	modal: any;
 
 	eventDate: string = new Date().toISOString();
-	distance: number = 1;
+	distance: number = 10;
 	typeOptions = [{
 		label: "Concert",
 		icon: "musical-notes",
@@ -60,11 +60,42 @@ export class NearEventsPage
 	filterOptions = { types: true, distance: true, date: true };
 
 	constructor( public navCtrl: NavController, private platform: Platform,
-		public modalCtrl: ModalController, private eventService: EventService )
+		public modalCtrl: ModalController, private eventService: EventService,
+		public loadingCtrl: LoadingController, private translateService: TranslateService )
 	{
 		this.platform.ready().then( () => {
 			this.getPosition();
 		} );
+	}
+
+	private isWithinRadius( latitude: number, longitude: number )
+	{
+		let eventPosition = new google.maps.LatLng( latitude, longitude );
+		let distance = google.maps.geometry.spherical.computeDistanceBetween( this.currentPosition, eventPosition );
+		if( distance < this.distance * 1000 )
+			return true;
+		return false;
+	}
+
+	private isSameDay( eventDate: Date )
+	{
+		let filterDate = new Date( this.eventDate );
+		if( filterDate.getDate() + 1 === eventDate.getDate() )
+			if( filterDate.getMonth() === eventDate.getMonth() )
+				if( filterDate.getFullYear() === eventDate.getFullYear() )
+					return true;
+		return false;
+	}
+
+	private translateMessage( messageTranslate: string )
+	{
+		let message: string;
+		this.translateService.get( messageTranslate ).subscribe( value => 
+		{
+			message = value;
+		} );
+
+		return message;
 	}
 
 	drawMarkers()
@@ -87,13 +118,73 @@ export class NearEventsPage
 	{
 		this.modal = this.modalCtrl.create( FilterPage, { filterOptions: this.filterOptions, typeOptions: this.typeOptions, distance: this.distance, date: this.eventDate } );
 		this.modal.onDidDismiss( data => {
+			let loading = this.loadingCtrl.create(
+			{
+				content: this.translateMessage( "NEAR_EVENTS.FILTERING_EVENTS" )
+			} );
+			loading.present();
+
 			this.typeOptions = data.typeOptions;
 			this.distance = data.distance;
 			this.eventDate = data.date;
 			this.filterOptions = data.filterOptions;
+
+			let firstTime = true;
+			let events = [];
+			if( this.filterOptions.types )
+			{
+				firstTime = false;
+				let types = {};
+				for( let i = 0; i < this.typeOptions.length; ++i )
+					types[this.typeOptions[i].value] = this.typeOptions[i].filter;
+
+				for( let i = 0; i < this.eventService.events.length; ++i )
+					if( types[this.eventService.events[i].type] )
+						events.push( this.eventService.events[i] );
+			}
+			if( this.filterOptions.distance )
+			{
+				if( firstTime )
+				{
+					for( let i = 0; i < this.eventService.events.length; ++i )
+						if( this.isWithinRadius( this.eventService.events[i].latitude, this.eventService.events[i].longitude ) )
+							events.push( this.eventService.events[i] );
+				}
+				else
+				{
+					for( let i = 0; i < events.length; ++i )
+						if( !this.isWithinRadius( events[i].latitude, events[i].longitude ) )
+						{
+							events.splice( i, 1 );
+							--i;
+						}
+				}
+
+				firstTime = false;
+			}
+			if( this.filterOptions.date )
+			{
+				if( firstTime )
+				{
+					for( let i = 0; i < this.eventService.events.length; ++i )
+						if( this.isSameDay( new Date( this.eventService.events[i].startDateTime ) ) )
+							events.push( this.eventService.events[i] );
+				}
+				else
+				{
+					for( let i = 0; i < events.length; ++i )
+						if( !this.isSameDay( new Date( events[i].startDateTime ) ) )
+						{
+							events.splice( i, 1 );
+							--i;
+						}
+				}
+			}
 			
-			this.eventService.eventsFilter = data.events;
+			this.eventService.eventsFilter = events;
 			this.drawMarkers();
+
+			loading.dismiss();
 		} );
 		this.modal.present();
 	}
@@ -143,7 +234,7 @@ export class NearEventsPage
 
 		this.map = new google.maps.Map( mapElement,
 		{
-			center: this.latLng,
+			center: this.currentPosition,
 			zoom: 13,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			tilt: 30
@@ -151,7 +242,7 @@ export class NearEventsPage
 
 		new google.maps.Marker(
 		{
-			position: this.latLng,
+			position: this.currentPosition,
 			icon: "assets/images/blue-circle.png",
 			map: this.map
 		} );
@@ -160,7 +251,7 @@ export class NearEventsPage
 			mapElement.classList.add( "show-map" );
 			google.maps.event.trigger( mapElement, "resize" );
 
-			let positionCoords = { latitude: this.latLng.lat(), longitude: this.latLng.lng() };
+			let positionCoords = { latitude: this.currentPosition.lat(), longitude: this.currentPosition.lng() };
 			this.eventService.getEvents( positionCoords ).then( response => {
 				let events = new Array();
 
@@ -190,7 +281,7 @@ export class NearEventsPage
 			let latitude = position.coords.latitude;
 			let longitude = position.coords.longitude;
 
-			this.latLng = new google.maps.LatLng( latitude, longitude );
+			this.currentPosition = new google.maps.LatLng( latitude, longitude );
 			
 			this.loadMap();
 		} ).catch( response => {
