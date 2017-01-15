df403407f33f638c9739962d710ae54b6322ebc0
jorge52an <jorgesolavila@gmail.com>
2017-01-15 13:48:23 -0500

    Chore: show events by user

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Platform, NavController } from "ionic-angular";
+import { NavController } from "ionic-angular";
 
 import { User, UserService } from "../../services/user.service";
 
