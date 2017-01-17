cf3adfd863bd22423d2b19d450b59046cd90278b
jorge52an <jorgesolavila@gmail.com>
2017-01-16 22:10:31 -0500

    Fix: get user information

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { App, NavController, AlertController } from "ionic-angular";
+import { App, Platform, NavController, AlertController } from "ionic-angular";
 
 import { User, UserService } from "../../services/user.service";
 import { TranslateServiceLocal } from "../../services/translate.service";
@@ -15,10 +15,14 @@ export class AccountPage
 {
 	private user: User = this.userService.getUserInfo();
 
-	constructor( private app: App, public navCtrl: NavController, 
-		private alertCtrl: AlertController, private userService: UserService, 
-		private translateService: TranslateServiceLocal )
+	constructor( private app: App, public platform: Platform, 
+		public navCtrl: NavController, private alertCtrl: AlertController, 
+		private userService: UserService, private translateService: TranslateServiceLocal )
 	{
+		this.platform.ready().then( () => 
+		{
+			this.user = this.userService.getUserInfo();
+		} );
 	}
 
 	public logout(): void
